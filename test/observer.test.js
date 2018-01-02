4771a42138b1ed591ef38c582ff6dd1ffe0df6fb
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-01 22:05:55 -0500

    Build out initial tests for 'observer/pub-sub.js.'

--- test/observer.test.js
+++ test/observer.test.js
@@ -55,3 +55,43 @@ test( 'Subject: `publish()` method should error if invoked without a `data` argu
 
 	t.throws( () => { subject.publish() } );
 } );
+
+test( 'PubSub: It should be importable.', ( t ) => {
+	t.is( typeof PubSub, 'function' );
+} );
+
+test( 'PubSub: It should be instantiable.', ( t ) => {
+	let pubSub = new PubSub;
+
+	t.is( pubSub instanceof PubSub, true );
+} );
+
+test( 'PubSub: It should expose the `subscribe() method`.', ( t ) => {
+	let pubSub = new PubSub();
+
+	t.is( typeof pubSub.subscribe, 'function' );
+} );
+
+test.todo( 'PubSub: `subscribe()` method should return a number.' );
+
+test.todo( 'PubSub: `subscribe()` method should error if invoked without a `topic` argument.' );
+
+test.todo( 'PubSub: `subscribe()` method should error if invoked without a `callback` argument.' );
+
+test( 'PubSub: It should expose the `publish() method`.', ( t ) => {
+	let pubSub = new PubSub();
+
+	t.is( typeof pubSub.publish, 'function' );
+} );
+
+test.todo( 'PubSub: `publish()` method should error if invoked without a `topic` argument.' );
+
+test.todo( 'PubSub: `publish()` method should error if invoked without a `data` argument.' );
+
+test( 'PubSub: It should expose the `unsubscribe() method`.', ( t ) => {
+	let pubSub = new PubSub();
+
+	t.is( typeof pubSub.unsubscribe, 'function' );
+} );
+
+test.todo( 'PubSub: `unsubscribe()` method should error if invoked without an `id` argument.' );
