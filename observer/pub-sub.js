3136aff0a20517c89c4ca8a989407b0a4fa51ff3
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-01 21:29:22 -0500

    Observer: complete first pass of 'pub-sub' implementation.

---
+++ observer/pub-sub.js
@@ -0,0 +1,71 @@
+// --------------------------------------------------
+// IMPORT MODULES
+// --------------------------------------------------
+// Node
+// Vendor
+// Project
+
+// --------------------------------------------------
+// DECLARE VARS
+// --------------------------------------------------
+
+// --------------------------------------------------
+// DECLARE FUNCTIONS
+// --------------------------------------------------
+
+// ...
+// DEFINE CLASS
+// EXPOSE METHODS
+	// #subscribe( topic, callback )
+	// #publish( topic, data )
+	// #unsubscribe( id )
+
+class PubSub {
+	constructor() {
+		this.topics = {};
+	}
+
+	subscribe( topic, callback ) {
+		if ( !this.topics[ topic ] ) {
+			this.topics[ topic ] = [];
+		}
+
+		let observer = {
+			id: new Date().getTime(),
+			callback,
+		};
+
+		this.topics[ topic ].push( observer );
+
+		return observer.id;
+	}
+
+	publish( topic, data ) {
+		if ( this.topics[ topic ] ) {
+			this.topics[ topic ].forEach( observer => observer.callback( data ) );
+		}
+	}
+
+	unsubscribe( id ) {
+		let removedObserver = false;
+
+		// NOTE: Since we cannot use break/continue within `forEach()`, `removedObserver` allows us to proceed or skip subsequent checks.
+		Object.keys( this.topics ).forEach( topic => {
+			if ( !removedObserver ) {
+				this.topics[ topic ].forEach( ( observer, index ) => {
+					if ( !removedObserver ) {
+						if ( observer.id === id ) {
+							removedObserver = true;
+							this.topics[ topic ].splice( index, 1 );
+						}
+					}
+				} );
+			}
+		} );
+	}
+}
+
+// --------------------------------------------------
+// PUBLIC API
+// --------------------------------------------------
+module.exports = PubSub;
